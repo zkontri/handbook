4a55ea44c4a82ceaffc633d6564eb29865b7687e
UltralyticsAssistant <web@ultralytics.com>
2025-05-05 19:34:22 +0000

    Auto-format by https://ultralytics.com/actions

--- docs/overrides/javascript/extra.js
+++ docs/overrides/javascript/extra.js
@@ -164,9 +164,25 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Fix missing trailing slash on language home pages
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const path = window.location.pathname;
-  if (['/zh', '/ko', '/ja', '/ru', '/de', '/fr', '/it', '/es', '/pt', '/tr', '/vi', '/ar'].includes(path)) {
-    window.location.href = path + '/' + window.location.search + window.location.hash;
+  if (
+    [
+      "/zh",
+      "/ko",
+      "/ja",
+      "/ru",
+      "/de",
+      "/fr",
+      "/it",
+      "/es",
+      "/pt",
+      "/tr",
+      "/vi",
+      "/ar",
+    ].includes(path)
+  ) {
+    window.location.href =
+      path + "/" + window.location.search + window.location.hash;
   }
 });
